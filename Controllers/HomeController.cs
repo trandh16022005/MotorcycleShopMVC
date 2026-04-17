2d9fa2eba0509a01a70b2644ee89df4dd503dbd4
trandh1602 <tranhoa712005@gmail.com>
2026-04-17 11:26:34 +0700

    Add home UI + layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MotorcycleShopMVC.Models;
-using System.Diagnostics;
 
 namespace MotorcycleShopMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? brandId)
         {
-            return View();
-        }
+            var brands = await _context.Brands.ToListAsync();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            var motorcycles = _context.Motorcycles
+                .Include(m => m.Brand)
+                .OrderByDescending(m => m.CreatedAt)
+                .AsQueryable();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (brandId != null)
+            {
+                motorcycles = motorcycles.Where(m => m.BrandId == brandId);
+            }
+
+            var result = await motorcycles.Take(8).ToListAsync();
+
+            ViewBag.Brands = brands;
+            ViewBag.SelectedBrand = brandId;
+
+            return View(result);
         }
     }
 }
