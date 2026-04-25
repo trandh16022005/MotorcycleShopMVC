e32aa4e574b3391c6861800beea7a3fbefcf37fb
trandh1602 <tranhoa712005@gmail.com>
2026-04-25 14:57:00 +0700

    fix conflict MotorcycleController

--- Controllers/MotorcycleController.cs
+++ Controllers/MotorcycleController.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Linq;
-=======
 using System.Linq;
->>>>>>> origin/Mh
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
